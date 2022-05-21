d90db8dc79e6494cc566d87247ee1e9aa7a37848
Serhii Vatulyev <4845637@gmail.com>
2022-05-21 08:17:56 +0300

    3 commit

--- components/MainContainer.js
+++ components/MainContainer.js
@@ -1,14 +1,16 @@
 import A from "./A";
 import Head from "next/head";
+import NavbarElement from "./Navbar";
 
 const MainContainer = ({children,keywords}) => {
     const title = 'title'
     return (
         <>
             <Head>
-                <meta keywords={keywords}/>
+                <meta keywords={keywords} />
                 <title>{title}</title>
             </Head>
+            <NavbarElement/>
             <div className={"navbar"}>
                 <A href={'/'} text="main" />
                 <A href={'/users'} text="users" />
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import A from "../components/A";
 import MainContainer from "../components/MainContainer";
+//import {Button} from "@mui/material";
 
 const Index = () => {
     return (
