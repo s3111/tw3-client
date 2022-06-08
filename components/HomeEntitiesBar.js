45660eb2b106cd182afa1c49e0efdae24719c28a
Serhii Vatulyev <4845637@gmail.com>
2022-06-08 18:58:07 +0300

    commit

--- components/HomeEntitiesBar.js
+++ components/HomeEntitiesBar.js
@@ -10,14 +10,13 @@ export default function HomeEntitiesBar({entities}) {
             {entities.map(i =>
                 <Card
                     key={i.entityId}
-                    className="p-2 m-1 d-inline-block"
-                    //style={{cursor: 'pointer'}}
-                    border={'light'}
+                    //className={"p-2 pr-3 m-1 d-inline-block " + styles.card}
+                    className={"d-inline-block m-2 p-2 px-3"}
                 >
                     <Row>
                         <Col>
                             <Row>
-                                <Col>
+                                <Col className={styles.entity}>
                                     <Link href={`/tweets/${i.entity}`}>
                                         <a>
                                             {i.entity}
@@ -26,15 +25,21 @@ export default function HomeEntitiesBar({entities}) {
                                 </Col>
                             </Row>
                             <Row>
-                                <Col>
-                                    <div className={"text-secondary"}>{i.type.toLowerCase()}</div>
+                                <Col className={styles.type}>
+                                    <div>
+                                        <Link href={`/entities/${i.type}`}>
+                                            <a>
+                                                {i.type.toLowerCase()}
+                                            </a>
+                                        </Link>
+                                    </div>
                                 </Col>
                             </Row>
                         </Col>
                         <Col className={"p-1"}>
                             <Link href={"/report"}>
                                 <a className={styles.persent}>
-                                    <span >{parseInt((i.weightIndex - 1) * 100)}%</span>
+                                    <span>{parseInt((i.weightIndex - 1) * 100)}%</span>
                                 </a>
                             </Link>
                         </Col>
